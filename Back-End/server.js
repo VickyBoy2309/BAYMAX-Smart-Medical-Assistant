73e85110910db2f757fc68e82b007ab0209aad0d
Vicky_Boy_23 <0987vickyboy@gmail.com>
2026-01-15 12:56:40 +0530

    I Changed Somthing

--- Back-End/server.js
+++ Back-End/server.js
@@ -27,4 +27,5 @@ mongoose.connect(process.env.MONGO_URI)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(process.env.PORT);
 });
